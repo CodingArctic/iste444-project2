4fe2d2f5760d47097430419eceba9d09cc00d290
ethan-logue <emlrcc@rit.edu>
2025-05-01 10:18:27 -0400

    Added skeleton components and contentProvider

---
+++ frontend/src/utils/ContentProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react';
+import Home from '../pages/Home';
+
+export const ContentContext = createContext();
+
+export const ContentProvider = ({ children }) => {
+  const [content, setContent] = useState(<Home />);
+
+  return (
+    <ContentContext.Provider value={{ content, setContent }}>
+      {children}
+    </ContentContext.Provider>
+  );
+};
+
+export const useContent = () => {
+    const context = useContext(ContentContext);
+    if (context === undefined) {
+      throw new Error('useContent must be used within a ContentProvider');
+    }
+    return context;
+  };
