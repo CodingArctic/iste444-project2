4474577411d2d88f3560270e415f84e01a2802bc
Alex Leute <alex@alexleute.com>
2025-05-02 23:31:45 -0400

    Create the database

--- backend/db/create_database.sql
+++ backend/db/create_database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Car (
     vin INTEGER PRIMARY KEY,
-    ownerId INTEGER REFERENCES User
+    ownerId INTEGER REFERENCES User,
     make TEXT,
     year INTEGER,
     MILAGE INTEGER,
-    price INTEGER,
-)
+    price INTEGER
+);
 
 CREATE TABLE User (
     id INTEGER PRIMARY KEY,
     username TEXT,
-    password BLOB,
-)
+    password BLOB
+);
